6d700fb032a9e3f131c785c548f371b9ec6c6bbb
remi <adeniyibadaaderemi4@gmail.com>
2025-01-19 13:26:24 +0100

    first git push

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-500 flex items-center justify-center">
+      <p className="bg-red-300">kkk</p>
+    </div>
+  );
+}
+
+export default App;
