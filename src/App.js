4478467f9073bca40dc33a24bf58c4f58c61d73d
remi <adeniyibadaaderemi4@gmail.com>
2025-01-20 02:37:59 +0100

    git initial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import "./index.css";
+import React from 'react'
+
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-500 flex items-center justify-center">
-      <p className="bg-red-300">kkk</p>
+    <div >
     </div>
   );
 }
